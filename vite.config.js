39e281935838c12284359c101bb98f273a60218a
sonu bhosle <143890614+sonubhosle@users.noreply.github.com>
2025-02-01 10:41:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,3 +7,35 @@ export default defineConfig({
 
 
 })
+
+
+export const food = [
+    {
+        "id": 1,
+        "name": "Jai Ganesh Bhojnalaya",
+        "brand": "Zomato",
+        "image": "https://b.zmtcdn.com/data/dish_photos/761/c8296b818dfdfde5ab079b98bc9dc761.jpg?output-format=webp",
+        "price": 150,
+        "discountedPrice": 135,
+        "discount": 10,
+        "desc": "North Indian",
+        "category": "Meggi",
+        "quantity": 2,
+        "rating": "4.5",
+        "review": "1,999"
+    },
+    {
+        "id": 2,
+        "name": "Shree Balaji Chinese Centre",
+        "brand": "Swiggy",
+        "image": "https://b.zmtcdn.com/data/pictures/3/19849373/6ea3ff51fcea619598a7a86096886edb_o2_featured_v2.jpg?output-format=webp",
+        "price": 150,
+        "discountedPrice": 135,
+        "discount": 10,
+        "desc": "North Indian",
+        "category": "Pizza",
+        "quantity": 2,
+        "rating": "4.5",
+        "review": "1,999"
+    },
+]
