01a5982f3b2fe0fab46a065fdbd0edaaee54df4e
sonubhosle <sbhosle1011@gmail.com>
2025-02-04 15:47:22 +0530

    Evening 3.45

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Heading.css'
 
-const Heading = ({heading,subtitle}) => {
+
+const Heading = ({ heading, subtitle }) => {
   return (
     <div className='heading_section'>
-        <h2>{heading}</h2>
-        <p>{subtitle}</p>
+      <h2>{heading}</h2>
+      <p>{subtitle}</p>
     </div>
   )
 }
--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import Heading from '../../Components/Heading/Heading'
+import './Career.css'
+const companyBenefits = [
+  {
+    title: "Growth Opportunities",
+    description: "Develop your skills through challenging projects and learn from industry experts."
+  },
+  {
+    title: "Innovative Work Environment",
+    description: "Join a culture that fosters creativity and embraces new technology."
+  },
+  {
+    title: "Competitive Compensation",
+    description: "Enjoy an attractive salary package with industry-leading benefits."
+  }
+]
 const Career = () => {
   return (
-    <div>Career</div>
+    <div className='career'> 
+      <Heading heading={'Join Our Team'} subtitle={"We’re always looking for talented and motivated individuals. Whether you're an experienced professional or a new graduate, we offer growth opportunities to help you achieve your goals."} />
+       <div className="grid_comp">
+        {
+          companyBenefits.map((cmp,indx) => {
+            return(
+              <div className="comp_card">
+                <div className="heading">{cmp.title}</div>
+                <div className="desc">{cmp.description}</div>
+              </div>
+            )
+          })
+        }
+       </div>
+    </div>
   )
 }
 
