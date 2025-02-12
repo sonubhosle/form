1eb1afaae8302b63f08c721bbe212f5ce4d0ebef
sonubhosle <sbhosle1011@gmail.com>
2025-02-12 13:13:32 +0530

    1PM

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import './Navigation.css'
+import React, { useEffect, useState } from 'react';
+import './Navigation.css';
 import { Link } from 'react-router-dom';
 import { PiStudentBold } from "react-icons/pi";
-
 import { FaRegHandPointLeft } from "react-icons/fa";
 
 const Navigation = () => {
-
   const [scrolling, setScrolling] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleScroll = () => {
     if (window.scrollY > 50) {
@@ -22,35 +21,46 @@ const Navigation = () => {
     setIsProfileOpen(prev => !prev);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
   return (
     <header className={`navbar ${scrolling ? 'scrolled' : ''}`}>
       <Link className="logo">
-      <div className="icon"><PiStudentBold color='#2D336B' size={50} /></div>
-      <div className="details">
-        <div className="name">SKY BRICKS</div>
-        <div className="slogan">Think Develop Achieve</div>
-      </div>
+        <div className="icon"><PiStudentBold color='#2D336B' size={50} /></div>
+        <div className="details">
+          <div className="name">SKY BRICKS</div>
+          <div className="slogan">Think Develop Achieve</div>
+        </div>
       </Link>
 
-      <div className="meuns flex items-center gap-20">
+      <div className={`meuns ${menuOpen ? 'show-menu' : ''} flex items-center gap-20`}>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/contact'>Contact</Link>
-        <Link to="/pricing" className='color'> Pricing</Link>
+        <Link to="/pricing" className='color'>Pricing</Link>
         <Link to='/career'>Career</Link>
         <Link to='/projects'>Projects</Link>
-        <Link to='/apply' className='button'>    Apply Now <p><FaRegHandPointLeft size={24} color='white'/></p></Link>
+        <Link to='/apply' className='button'>
+          Apply Now <p><FaRegHandPointLeft size={24} color='white' /></p>
+        </Link>
       </div>
 
+      <div className={`hamburger-menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </header>
+  );
+};
 
-  )
-}
-
-export default Navigation
+export default Navigation;
