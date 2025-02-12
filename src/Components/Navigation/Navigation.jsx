b2762af4ceb07a23b09d5d0f3cf8316f51e0558e
sonubhosle <sbhosle1011@gmail.com>
2025-02-12 14:38:25 +0530

    2:30

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -3,37 +3,20 @@ import './Navigation.css';
 import { Link } from 'react-router-dom';
 import { PiStudentBold } from "react-icons/pi";
 import { FaRegHandPointLeft } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const Navigation = () => {
-  const [scrolling, setScrolling] = useState(false);
-  const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleScroll = () => {
-    if (window.scrollY > 50) {
-      setScrolling(true);
-    } else {
-      setScrolling(false);
-    }
-  };
-
-  const toggleProfileMenu = () => {
-    setIsProfileOpen(prev => !prev);
-  };
 
   const toggleMenu = () => {
     setMenuOpen(prev => !prev);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
-    <header className={`navbar ${scrolling ? 'scrolled' : ''}`}>
+    <header className='navbar'>
       <Link className="logo">
         <div className="icon"><PiStudentBold color='#2D336B' size={50} /></div>
         <div className="details">
@@ -42,6 +25,7 @@ const Navigation = () => {
         </div>
       </Link>
 
+      {/* Menu Links - These will show only if the menu is open */}
       <div className={`meuns ${menuOpen ? 'show-menu' : ''} flex items-center gap-20`}>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
@@ -54,10 +38,9 @@ const Navigation = () => {
         </Link>
       </div>
 
+      {/* Hamburger Icon - This shows when the menu is closed */}
       <div className={`hamburger-menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
-        <div></div>
-        <div></div>
-        <div></div>
+        {!menuOpen ? <IoMenu className='menu_icon' size={30}/> : <IoClose className='close_icon' size={30}/>}
       </div>
     </header>
   );
